427ee2bff4342c8df248b52e531e066de6dec63f
Daniel-615 <suyanangel615@gmail.com>
2025-10-15 18:50:30 -0600

    setting true secure

--- src/middleware/cookieOptions.js
+++ src/middleware/cookieOptions.js
@@ -1,9 +1,6 @@
-const {NODE_ENV } = require("../config/config.js");
-const isProduction = NODE_ENV === "production";
-
 module.exports = {
   httpOnly: true,
-  secure: isProduction, 
-  sameSite: isProduction ? "none" : "lax",
+  secure: true, 
+  sameSite: "none",
   path: "/", 
 };
